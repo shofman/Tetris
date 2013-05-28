23b1c34b669bdd05bd33857c0e4ae492eebbff35
Scott Hofman <stumpitron@gmail.com>
2013-05-28 03:29:43 +0100

    Basic Collision detection

--- Tetris.py
+++ Tetris.py
@@ -8,11 +8,12 @@ blue = (0,0,255)
 green = (0,255,0)
 yellow = (0, 255, 255)
 purple = (255, 0, 255)
-other = (255, 255, 0)
+cyan = (255, 255, 0)
+orange = (255, 140, 0)
+pileBlocks = []
 
 
 class Block:
-    
     def __init__(self):
         randomblock = random.randint(1, 7)
         self.position = self.randomBlock(randomblock)
@@ -21,10 +22,11 @@ class Block:
 
     def draw(self):
         for block in self.position:
-            self.drawBlock(self.color, black, (block[0]*30, block[1]*30, 30, 30))
+            self.drawBlock(self.color, black, getBlockLocation(block))
 
     def fall(self):
-        if any(block[1] > 16 for block in self.position):
+        #If bottom is reached, or will collide with another block next turn
+        if any(block[1] > 17 for block in self.position) or any((block[0], block[1] + 1 ) in [blocks[0] for blocks in pileBlocks] for block in self.position):
             self.falling = False
         if self.falling:
             self.position = [(block[0], block[1] + 1) for block in self.position]
@@ -42,20 +44,20 @@ class Block:
             return [(initpos[0], initpos[1]), (initpos[0], initpos[1]+1), (initpos[0], initpos[1]+2), (initpos[0]-1, initpos[1]+2)]
         elif randomblock == 5: #Middle
             return [(initpos[0], initpos[1]), (initpos[0]-1, initpos[1]), (initpos[0]+1, initpos[1]), (initpos[0], initpos[1]+1)]
-        elif randomblock == 6:
+        elif randomblock == 6: #Squigle Left
             return [(initpos[0], initpos[1]), (initpos[0]-1, initpos[1]), (initpos[0]+1, initpos[1]+1), (initpos[0], initpos[1]+1)]
-        else:
+        else: #Squigle Right
             return [(initpos[0], initpos[1]), (initpos[0]+1, initpos[1]), (initpos[0]-1, initpos[1]+1), (initpos[0], initpos[1]+1)]
 
     def randomColor(self, randomblock):
         if randomblock == 1:
             return red
         elif randomblock == 2:
-            return black
+            return orange
         elif randomblock == 3:
             return purple
         elif randomblock == 4:
-            return other
+            return cyan
         elif randomblock == 5:
             return blue
         elif randomblock == 6:
@@ -71,32 +73,31 @@ class Block:
         print self.position
 
     def moveLeft(self):
-        if all(block[0] >= 2 for block in self.position) and self.falling:
+        if all(block[0] >= 2 for block in self.position) and self.falling and not any((block[0] - 1, block[1]) in [blocks[0] for blocks in pileBlocks] for block in self.position):
             self.position = [(block[0] - 1, block[1]) for block in self.position]
 
     def moveRight(self):
-        if all(block[0] <= 9 for block in self.position) and self.falling:
+        if all(block[0] <= 9 for block in self.position) and self.falling and not any((block[0] + 1, block[1]) in [blocks[0] for blocks in pileBlocks] for block in self.position):
             self.position = [(block[0] + 1, block[1]) for block in self.position]
 
-
+def getBlockLocation(blockLoc):
+    return (blockLoc[0]*30, blockLoc[1]*30, 30, 30)
 
 
 def drawPile(pileBlocks):
     for block in pileBlocks:
-        pygame.draw.rect(screen, block[1], (block[0][0]*30, block[0][1]*30, 30, 30), 0)
-        pygame.draw.rect(screen, black, (block[0][0]*30, block[0][1]*30, 30, 30), 1)
+        pygame.draw.rect(screen, block[1], getBlockLocation(block[0]), 0)
+        pygame.draw.rect(screen, black, getBlockLocation(block[0]), 1)
     
 def run():
     clocker = pygame.time.Clock()
     fallspeed = 50
-    temp = Block()
+    fallBlock = Block()
     time = 0
-    pileBlocks = []
-        
+    
     while 1:
         clocker.tick()
         time += clocker.get_time()
-        #firstblock = (initpos[0], initpos[1] + fallspeed * float(time)/1000)
         
         screen.fill((255, 255, 255))
         for event in pygame.event.get():
@@ -105,29 +106,23 @@ def run():
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_n:
-                    temp = Block()
+                    fallBlock = Block()
                 elif event.key == pygame.K_m:
-                    temp.showPos()
+                    fallBlock.showPos()
                 elif event.key == pygame.K_LEFT:
-                    temp.moveLeft()
+                    fallBlock.moveLeft()
                 elif event.key == pygame.K_RIGHT:
-                    temp.moveRight()
-                
-              #      initpos = (initpos[0] - 30, initpos[1])
-               # elif event.key == pygame.K_RIGHT:
-                #    initpos = (initpos[0] + 30, initpos[1])
-        #print time
-        #drawBlock(red, black, (initpos[0], initpos[1], 30,30))
+                    fallBlock.moveRight()
+
         if (float(time) /10 > fallspeed):
             time = 0
-            if not temp.fall():
-                for block in temp.position:
-                    pileBlocks.append((block,temp.color))
-                    print pileBlocks
-                temp = Block()
+            if not fallBlock.fall():
+                for block in fallBlock.position:
+                    pileBlocks.append((block, fallBlock.color))
+                fallBlock = Block()
 
-        temp.draw()
+        fallBlock.draw()
         drawPile(pileBlocks)
-        #drawBlock(blue, black, (200, 320, 30, 30))
+                                                                
         pygame.display.flip()
         
